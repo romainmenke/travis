e70f66b8b0ad803969202c6046f35a89adb0ad7f
romainmenke <romainmenke@gmail.com>
2016-07-02 15:14:10 +0200

    initial

---
+++ payload.go
@@ -0,0 +1,74 @@
+package travis
+
+// BodyObject is the top level object received from a Travis CI webhook
+type BodyObject struct {
+	Payload PayloadObject `json:"payload"`
+}
+
+// PayloadObject contains the actual data from Travis
+type PayloadObject struct {
+	ID             string `json:"id"`
+	Number         string `json:"number"`
+	Status         string `json:"status"`
+	StartedAt      string `json:"started_at"`
+	FinishedAt     string `json:"finished_at"`
+	StatusMessage  string `json:"status_message"`
+	Commit         string `json:"commit"`
+	Branch         string `json:"branch"`
+	Message        string `json:"message"`
+	CompareURL     string `json:"compare_url"`
+	CommittedAt    string `json:"committed_at"`
+	CommitterName  string `json:"committer_name"`
+	CommitterEmail string `json:"committer_email"`
+	AuthorName     string `json:"author_name"`
+	AuthorEmail    string `json:"author_email"`
+	Type           string `json:"type"`
+	BuildURL       string `json:"build_url"`
+
+	Repository RepositoryObject `json:"repository"`
+	Config     ConfigObject     `json:"config"`
+	Matrix     []MatrixElement  `json:"matrix"`
+}
+
+// RepositoryObject contains the repo data
+type RepositoryObject struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	OwnerName string `json:"owner_name"`
+	URL       string `json:"url"`
+}
+
+// ConfigObject contains the notification data
+type ConfigObject struct {
+	Notifications NotificationsObject `json:"notifications"`
+}
+
+// NotificationsObject contains a list of webhooks
+type NotificationsObject struct {
+	WebHooks []string `json:"webhooks"`
+}
+
+// MatrixElement contains the data for each Travis Matrix object
+type MatrixElement struct {
+	ID             string `json:"id"`
+	RepositoryID   string `json:"repository_id"`
+	Number         string `json:"number"`
+	State          string `json:"state"`
+	StartedAt      string `json:"started_at"`
+	FinishedAt     string `json:"finished_at"`
+	Status         string `json:"status"`
+	Log            string `json:"log"`
+	Result         string `json:"result"`
+	ParentID       string `json:"parent_id"`
+	Commit         string `json:"commit"`
+	Branch         string `json:"branch"`
+	Message        string `json:"message"`
+	CommittedAt    string `json:"committed_at"`
+	CommitterName  string `json:"committer_name"`
+	CommitterEmail string `json:"committer_email"`
+	AuthorName     string `json:"author_name"`
+	AuthorEmail    string `json:"author_email"`
+	CompareURL     string `json:"compare_url"`
+
+	Config ConfigObject `json:"config"`
+}
---
+++ travis.go
@@ -0,0 +1,46 @@
+package travis
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"limbo.services/trace"
+
+	"github.com/gorilla/mux"
+	"golang.org/x/net/context"
+)
+
+var callBack func(payload *PayloadObject)
+
+func HandleTravisWebHook(r *mux.Router, path string, f func(payload *PayloadObject)) {
+	callBack = f
+	r.HandleFunc(path, ReceiveTravis)
+}
+
+func ReceiveTravis(w http.ResponseWriter, r *http.Request) {
+
+	ctx := context.Background()
+
+	span, ctx := trace.New(ctx, "travis.receive")
+	defer span.Close()
+
+	var bodyObject BodyObject
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		span.Error(err)
+	}
+	defer r.Body.Close()
+
+	err = json.Unmarshal(body, &bodyObject)
+	if err != nil {
+		span.Error(err)
+	}
+
+	if callBack == nil {
+		span.Error("bad callback")
+	}
+	callBack(&bodyObject.Payload)
+
+}
